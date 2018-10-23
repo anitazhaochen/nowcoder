1fb6781bcde36623ccc19d08d5a1c3c24a3a3077
zhao <--global>
2018-10-23 17:30:50 +0800

    幸运数代码改进

--- 幸运数.py
+++ 幸运数.py
@@ -5,12 +5,7 @@
 n = input()
 count = 0
 for i in range(1,n+1):
-    n1 = map(int,list(str(bin(i)))[2:])
-    n1 = sum(n1)
-    n2 = map(int,list(str(i)))
-    n2 = sum(n2)
-    if n1 == n2:
+    if sum(map(int,list(str(bin(i)))[2:])) == sum(map(int,list(str(i)))):
         count += 1
-
 print count
 
