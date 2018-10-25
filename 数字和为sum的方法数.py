d95a2ab160e77ab2b67e2ea395dc2329e851c93c
zhao <--global>
2018-10-25 11:24:40 +0800

    数字和为sum的方法数（动态规划版本过了）

--- 数字和为sum的方法数.py
+++ 数字和为sum的方法数.py
@@ -12,7 +12,26 @@ def count(sum, arr):
     sum2 += count(sum-arr[start],arr[start+1:])
     return sum1+sum2
 
-sum = int(raw_input().split(' ')[1])
-arr = map(int,raw_input().split(' '))
-print count(sum, arr)
+#sum = int(raw_input().split(' ')[1])
+#arr = map(int,raw_input().split(' '))
+#print count(sum, arr)
 
+def res(nums, sum):
+    dp = [ [0]*(sum+1) for _ in range(len(nums)+1)]
+    for i in range(0,len(nums)+1):
+        dp[i][0] = 1
+
+
+	count = 1
+    for j in nums:
+        for i in range(1,sum+1):
+            if i >= j:
+                dp[count][i] = dp[count - 1][i - j] + dp[count - 1][i]
+            else:
+                dp[count][i] = dp[count-1][i]
+        count += 1
+    print dp
+    print dp[-1][-1]
+
+
+res([5,5,10,2,3],15)
